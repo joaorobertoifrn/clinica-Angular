b25a3fb105768ad93a00967e0339944cb78d01ab
João Roberto de França Pereira <joao.roberto@academico.ifrn.edu.br>
2018-07-31 21:21:29 -0300

    Organização do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { SidemenuComponent } from './sidemenu/sidemenu.component';
-import { ControlsidebarComponent } from './controlsidebar/controlsidebar.component';
-import { HomeComponent } from './home/home.component';
-import { PacienteComponent } from './paciente/paciente.component';
-import { ProfissionalComponent } from './profissional/profissional.component';
-import { LocalatendimentoComponent } from './localatendimento/localatendimento.component';
-import { EspecialidadeComponent } from './especialidade/especialidade.component';
-import { UsuarioComponent } from './usuario/usuario.component';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { SidemenuComponent } from './template/sidemenu/sidemenu.component';
+import { HomeComponent } from './template/home/home.component';
+import { PacienteComponent } from './models/paciente/paciente.component';
+import { ProfissionalComponent } from './models/profissional/profissional.component';
+import { LocalatendimentoComponent } from './models/localatendimento/localatendimento.component';
+import { EspecialidadeComponent } from './models/especialidade/especialidade.component';
+import { UsuarioComponent } from './models/usuario/usuario.component';
 import { RouterModule } from '../../node_modules/@angular/router';
 import { LoginComponent } from './security/login/login.component';
 import { UtilModule } from './util/util.module';
@@ -23,7 +22,6 @@ import { UtilModule } from './util/util.module';
     HeaderComponent,
     FooterComponent,
     SidemenuComponent,
-    ControlsidebarComponent,
     HomeComponent,
     PacienteComponent,
     ProfissionalComponent,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { LoginComponent } from './security/login/login.component';
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { PacienteComponent } from './paciente/paciente.component';
-import { ProfissionalComponent } from './profissional/profissional.component';
-import { LocalatendimentoComponent } from './localatendimento/localatendimento.component';
-import { EspecialidadeComponent } from './especialidade/especialidade.component';
-import { UsuarioComponent } from './usuario/usuario.component';
+import { HomeComponent } from './template/home/home.component';
+import { PacienteComponent } from './models/paciente/paciente.component';
+import { ProfissionalComponent } from './models/profissional/profissional.component';
+import { LocalatendimentoComponent } from './models/localatendimento/localatendimento.component';
+import { EspecialidadeComponent } from './models/especialidade/especialidade.component';
+import { UsuarioComponent } from './models/usuario/usuario.component';
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
