7cd3aa030eae65d63881da668bf6a9ce4d1cf017
João Roberto de França Pereira <joao.roberto@academico.ifrn.edu.br>
2018-08-01 02:01:06 -0300

    Implementação do Interceptors

--- src/app/util/util.module.ts
+++ src/app/util/util.module.ts
@@ -1,12 +1,14 @@
 import {NgModule, ModuleWithProviders} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { InputComponent } from './input/input.component';
 import { RadioComponent } from './radio/radio.component';
 import { LoginService } from '../../services/login.service';
 import { NotificationService } from './messages/notification.service';
 import { BarranotificacaoComponent } from './barranotificacao/barranotificacao.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from '../security/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,8 @@ import { BarranotificacaoComponent } from './barranotificacao/barranotificacao.c
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   exports: [
     InputComponent,
@@ -32,7 +35,11 @@ export class UtilModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: UtilModule,
-      providers: [LoginService, NotificationService]
+      providers: [
+        LoginService,
+        NotificationService,
+        {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+      ]
     };
   }
 }
